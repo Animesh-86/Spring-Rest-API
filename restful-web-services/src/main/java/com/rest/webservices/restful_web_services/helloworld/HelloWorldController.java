6f5b6c20f1ca1dcb09de782b660b47cf7b220c09
Animesh <animeshiit0806@gmail.com>
2025-09-22 13:00:35 +0530

    API Versioning

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,9 +1,20 @@
 package com.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource  = messageSource;
+    }
+
 
     @GetMapping("/hello-world")
     public String helloWorld(){
@@ -18,6 +29,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Deafult Message", locale);
     }
 
 }
---
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -0,0 +1,44 @@
+package com.rest.webservices.restful_web_services.versioning;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+
+    //uri versioning
+    @GetMapping("/v1/person")
+    public PersonV1 getFirstVersionOfPerson(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping("/v2/person")
+    public PersonV2 getSecondVersionOfPerson(){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    //RequestParameter Versioning
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    //Content Header Versioning
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondtVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+
+}
