fdbdd172326972ba3aa83f3a85d296c49a93d827
Animesh <animeshiit0806@gmail.com>
2025-09-23 12:34:28 +0530

    Implemented HATEOS

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,12 +1,18 @@
 package com.rest.webservices.restful_web_services.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -22,12 +28,24 @@ public class UserResource {
         return service.findAll();
     }
 
+//    @GetMapping("/users/{id}")
+//    public User retrieveUser(@PathVariable int id){
+//        User user = service.findOne(id);
+//        if(user == null)
+//            throw new UserNotFoundException("id:"+id);
+//        return user;
+//    }
+
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null)
             throw new UserNotFoundException("id:"+id);
-        return user;
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
     }
 
     @DeleteMapping("/users/{id}")
